69d21a100d55b70a76933ea0e2644826a26d46fc
Kostiantyn Teltov <dneprokos@gmail.com>
2023-09-01 07:48:06 +0200

    Added XML and JSON parser examples

--- tests/genres/get_all_genres_test.py
+++ tests/genres/get_all_genres_test.py
@@ -1,9 +1,11 @@
+from json import loads
 from assertpy import assert_that, soft_assertions
 from api.genres_requests import GenresRequests
 import requests
+from jsonpath_ng import parse
 
 
-def test_get_all_genres_should_be_returned(regular_user_token):
+def test_get_all_genres_with_regular_user(regular_user_token):
     # Arrange
 
     # Act
@@ -23,3 +25,16 @@ def test_get_all_genres_should_be_returned(regular_user_token):
         # assert_that(response_body).extracting('name').contains('Action') # we can use contains() to check that the list contains the element
         assert_that(response_body).extracting('name').contains('Action', 'Horror', 'Romance') # we can use contains() to check that the list contains all the elements
 
+def test_get_all_genres_parse_json_expression(admin_user_token):
+    # Arrange
+
+    # Act
+    response = GenresRequests().get_all_genres(admin_user_token)
+
+    # Assert
+    assert_that(response.status_code).is_equal_to(requests.codes.OK)
+    genres = loads(response.text) # loads() converts a string to a dictionary
+    expression = parse('$[*].name') # parse() allows us to use JSONPath expressions to extract values from a dictionary
+    genres_names = [match.value for match in expression.find(genres)] # find() returns a list of matches
+    print(genres_names)
+        
